257306bdb78ad232ced37e1291b6fb0f29b4487e
Matvey Sergeev <47077074+Enhisir@users.noreply.github.com>
2023-09-11 20:37:44 +0300

    Сделал дз №1

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,40 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (args is null) 
+            throw new ArgumentNullException(nameof(args));
+
+        if (!IsArgLengthSupported(args)) 
+            throw new ArgumentException(
+                "Недостаточно аргументов, их должно быть не менее трех.",
+                nameof(args));
+        
+        if (!double.TryParse(args[0], out val1)) 
+            throw new ArgumentException(
+                "Первый операнд не является числом.", 
+                nameof(val1));
+
+        operation = ParseOperation(args[1]);
+        if (operation == CalculatorOperation.Undefined) 
+            throw new InvalidOperationException(nameof(operation));
+        
+        if (!double.TryParse(args[2], out val2)) 
+            throw new ArgumentException(
+                "Второй операнд не является числом.", 
+                nameof(val2));
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => CalculatorOperation.Undefined
+        };
     }
 }
