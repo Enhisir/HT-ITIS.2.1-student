ff95d3d73e2dfea7bbd4b70a25d67a9381a08311
Matvey Sergeev <47077074+Enhisir@users.noreply.github.com>
2023-09-25 10:35:35 +0300

    дз №3

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -3,9 +3,9 @@ namespace Tests.CSharp.Homework3;
 public class SingleInitializationSingleton
 {
     public const int DefaultDelay = 3_000;
-    private static readonly object Locker = new();
 
     private static volatile bool _isInitialized = false;
+    private static Lazy<SingleInitializationSingleton> _instance = new(isThreadSafe:true);
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
@@ -16,15 +16,29 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance => _instance.Value;
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        // check for avoiding redundant initialization and closure
+        var delay = _isInitialized ? Instance.Delay : DefaultDelay;
+        
+        _instance = new Lazy<SingleInitializationSingleton>(
+            () => new SingleInitializationSingleton(delay),
+            isThreadSafe: true);
+        _isInitialized = false;
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (_isInitialized) throw new InvalidOperationException();
+
+        // I don't implement DCL by myself
+        // because of guarantee of thread-safety
+        // when using Lazy<>
+        _instance = new Lazy<SingleInitializationSingleton>(
+            () => new SingleInitializationSingleton(delay),
+            isThreadSafe: true);
+        _isInitialized = true;
     }
 }
